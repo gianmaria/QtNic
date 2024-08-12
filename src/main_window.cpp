9987c094b4e4c86df4fcd9944d054fa17e2b0f91
Gianmaria <gianmaria.bonici@gmail.com>
2024-08-12 11:28:23 +0200

    using the new api

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -1,9 +1,11 @@
 #include "main_window.h"
 #include "./ui_main_window.h"
+#include <QDebug>
+#include <QPushButton>
+#include <QShortcut>
 
 #include "nic.h"
 
-#include <QPushButton>
 Main_Window::Main_Window(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Main_Window)
@@ -16,7 +18,20 @@ Main_Window::Main_Window(QWidget *parent)
 
     //ui->plainTextEdit->setPlainText(QString("User is admin? %1").arg(is_user_admin()));
 
-    vec<Interface> nics = collect_nic_info();
+    qDebug() << "is running as admin?" << is_running_as_administrator();
+
+
+    vec<std::shared_ptr<Interface>> nics = collect_nic_info();
+    for (const auto& nic: nics)
+    {
+        auto& cpp_str = get_name(nic);
+        ui->plainTextEdit->appendPlainText(QString::fromUtf8(cpp_str.data(), -1));
+        // qDebug() << "shared ptr counter:" << nic.use_count();
+    }
+
+
+    // ui->plainTextEdit->appendPlainText(QString("is user admin? %1").arg(is_running_as_administrator()));
+}
 
 void Main_Window::keyPressEvent(QKeyEvent *event)
 {
