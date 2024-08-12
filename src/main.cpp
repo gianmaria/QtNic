7f81efbdb532042c43eb7cdb6adcb51dace4b98e
Gianmaria <gianmaria.bonici@gmail.com>
2024-08-12 17:07:19 +0200

    check if we a re running as admin

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include "main_window.h"
 
 #include <QApplication>
+#include "nic.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    if (not is_running_as_administrator())
+    {
+        restart_as_admin();
+        qDebug() << "Adiosssssssssssss";
+        return 0;
+    }
 
     Main_Window w;
     // w.setWindowFlags(w.windowFlags() & ~Qt::WindowMaximizeButtonHint); // Remove maximize button
